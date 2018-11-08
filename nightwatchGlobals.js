92f4bbac832b41e368c6f8438189648062267b4b
Zach-Mayer <zach@zachmayer.net>
2018-11-08 16:53:52 +0530

    Babel dependencies are a pain

--- nightwatchGlobals.js
+++ nightwatchGlobals.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   // this controls whether to abort the test execution when an assertion failed and skip the rest
   // it's being used in waitFor commands and expect assertions
   abortOnAssertionFailure : false,
